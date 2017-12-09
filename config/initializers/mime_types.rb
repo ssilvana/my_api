c54c7da123c2cc437752a082c9b32674a9666ec1
Silvana Rosales <sil151283@gmail.com>
2017-12-09 14:28:20 -0300

    Create base app; Add users and posts

---
+++ config/initializers/mime_types.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Add new mime types for use in respond_to blocks:
+# Mime::Type.register "text/richtext", :rtf
+
+# this is an JSON:API compliant app
+Mime::Type.register "application/vnd.api+json", :json
